d339558c158674ac0325ef0868d251252cee3a8b
Philip Smith <philip.smith@nordcloud.com>
2016-09-22 00:56:12 +0100

    adding another group

--- security_groups.tf
+++ security_groups.tf
@@ -4,3 +4,10 @@ module "security_group" {
   sg_name = "${var.app_sg_name}"
   sg_description = "${var.app_sg_description}"
 }
+
+module "security_group" {
+  source = "github.com/electronicalias/tf-aws-sg"
+
+  sg_name = "${var.web_sg_name}"
+  sg_description = "${var.web_sg_description}"
+}
