24aa1e9ccfacecc7f0d4c7d0612f7cbf3e7c9c93
Philip Smith <philip.smith@nordcloud.com>
2016-09-22 08:22:35 +0100

    changing string interpolation

--- security_groups.tf
+++ security_groups.tf
@@ -1,13 +1,13 @@
 module "app_sg" {
   source = "github.com/electronicalias/tf-aws-sg"
 
-  sg_name = "${concat(var.app_sg_name, '-' var.environment)}"
-  sg_description = "${concat(var.app_sg_description, '-' var.environment)}"
+  sg_name = "${var.app_sg_name}-${var.environment}"
+  sg_description = "${var.app_sg_description}-${var.environment}"
 }
 
 module "web_sg" {
   source = "github.com/electronicalias/tf-aws-sg"
 
-  sg_name = "${concat(var.web_sg_name, '-' var.environment)}"
-  sg_description = "${concat(var.web_sg_description, '-' var.environment)}"
+  sg_name = "${var.web_sg_name}-${var.environment}"
+  sg_description = "${var.web_sg_description-${var.environment}"
 }
