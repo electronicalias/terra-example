68d0cbe8ec79ce52f69bfe3cf0779c1055d524da
Philip Smith <philip.smith@nordcloud.com>
2016-09-22 01:00:04 +0100

    change sg names

--- security_groups.tf
+++ security_groups.tf
@@ -1,11 +1,11 @@
-module "security_group" {
+module "app_sg" {
   source = "github.com/electronicalias/tf-aws-sg"
 
   sg_name = "${var.app_sg_name}"
   sg_description = "${var.app_sg_description}"
 }
 
-module "security_group" {
+module "web_sg" {
   source = "github.com/electronicalias/tf-aws-sg"
 
   sg_name = "${var.web_sg_name}"
