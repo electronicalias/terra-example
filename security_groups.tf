339e700ce241527c6eace487103dd6a303f38593
Philip Smith <philip.smith@nordcloud.com>
2016-09-28 23:07:23 +0100

    remove vpcid

--- security_groups.tf
+++ security_groups.tf
@@ -1,20 +1,17 @@
 module "app_sg" {
   source = "github.com/electronicalias/tf-aws-sg"
-  vpc_id = "${module.vpc.env.id}"
   sg_name = "${var.app_sg_name}-${var.environment_name}"
   sg_description = "${var.app_sg_description}-${var.environment_name}"
 }
 
 module "web_sg" {
   source = "github.com/electronicalias/tf-aws-sg"
-  vpc_id = "${module.vpc.env.id}"
   sg_name = "${var.web_sg_name}-${var.environment_name}"
   sg_description = "${var.web_sg_description}-${var.environment_name}"
 }
 
 module "db_sg" {
   source = "github.com/electronicalias/tf-aws-sg"
-  vpc_id = "${module.vpc.env.id}"
   sg_name = "${var.db_sg_name}-${var.environment_name}"
   sg_description = "${var.db_sg_description}-${var.environment_name}"
 }
