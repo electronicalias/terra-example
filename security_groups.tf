53eabd86a57b7ee8be641f17f481e72497f4dc5f
Philip Smith <philip.smith@nordcloud.com>
2016-09-28 21:30:01 +0100

    fix app/web/db naming

--- security_groups.tf
+++ security_groups.tf
@@ -11,3 +11,10 @@ module "web_sg" {
   sg_name = "${var.web_sg_name}-${var.environment_name}"
   sg_description = "${var.web_sg_description}-${var.environment_name}"
 }
+
+module "db_sg" {
+  source = "github.com/electronicalias/tf-aws-sg"
+
+  sg_name = "${var.db_sg_name}-${var.environment_name}"
+  sg_description = "${var.db_sg_description}-${var.environment_name}"
+}
