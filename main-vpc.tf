87f5d7c7a8681453157ddbff50f759454776e004
Philip Smith <philip.smith@nordcloud.com>
2016-09-29 20:50:24 +0100

    changing index

--- main-vpc.tf
+++ main-vpc.tf
@@ -16,14 +16,14 @@ module "vpc" {
 module "pci_vpc" {
   source = "github.com/electronicalias/tf_aws_vpc"
 
-  name = "${var.pci_vpc["name"]}"
+  name = "${var.pci_vpc['name']}"
   environment = "${var.environment_name}"
   enable_dns_hostnames = true
   enable_dns_support = true
 
   cidr = "${var.pci_vpc[0]}"
-  private_subnets = ["${var.pci_vpc["private_subnet01"]}"]
-  public_subnets  = ["${var.pci_vpc["public_subnet01"]}"]
+  private_subnets = ["${var.pci_vpc['private_subnet01']}"]
+  public_subnets  = ["${var.pci_vpc['public_subnet01']}"]
 
   azs      = ["eu-west-1a"]
 }
